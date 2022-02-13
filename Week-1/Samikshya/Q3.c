4af903796cebd2250bb8b7e96d019d6ed84a4eb3
Samikshya006 <99582489+Samikshya006@users.noreply.github.com>
2022-02-13 15:42:08 +0530

    Add files via upload

---
+++ Week-1/Samikshya/Q3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main()
+{
+	//prgrm to check whether a number is even or odd using switch case
+  int a;
+  printf("enter a number \n");
+  scanf("%d",&a);
+  switch (a%2){
+  	case 0:
+  	   printf("entered number is even\n");
+  	   break;
+  	default:
+  	   printf("entered number is odd \n");
+  	   break;
+  }
+	return 0;
+}
+
+ 
